fa52883db041604e77402a97d85990a3f30238a1
dfour001 <daniel.fourquet@gmail.com>
2022-02-04 10:36:14 -0500

    Handle multipart route geometries

--- Python/arcpy/find_line_mp.py
+++ Python/arcpy/find_line_mp.py
@@ -25,24 +25,43 @@
 
 def get_line_mp(inputPolyline, lrs, rte_nm):
     """ Locates the begin and end MP values of an input line along the LRS
-
         ** The spatial reference of the input must match the spatial reference
            of the lrs! **
-
     Input:
         inputPolyline - an arcpy Polyline object
         lrs - a reference to the lrs layer
         rte_nm - the lrs rte_nm that the polyline will be placed on
-
     Output:
         (beginMP, endMP)
     """
 
     try:
         # Get the geometry for the LRS route
+        RouteGeom = None
+        if lrs.getSelectionSet():
+            arcpy.management.SelectLayerByAttribute(lrs, 'CLEAR_SELECTION')
         with arcpy.da.SearchCursor(lrs, "SHAPE@", "RTE_NM = '{}'".format(rte_nm)) as cur:
             for row in cur:
                 RouteGeom = row[0]
+
+        if not RouteGeom:
+            print(f'Route "{rte_nm}" not found')
+            return None, None
+
+        # Check for multipart geometry.  If multipart, find closest part to
+        # ensure that the correct MP is returned
+        if geom.isMultipart:
+            # Get list of parts
+            parts = [arcpy.Polyline(RouteGeom[i], has_m=True) for i in range(RouteGeom.partCount)]
+
+            # Get input polyline midpoint
+            midPoint = inputPolyline.positionAlongLine(0.5, use_percentage=True)
+
+            # Get distances from inputPolyline's mid-point to each route part
+            partDists = {midPoint.distanceTo(part):part for part in parts}
+
+            # Replace RouteGeom with closest polyline part
+            RouteGeom = partDists[min(partDists)]
 
         def get_mp_from_point(route, point):
             """ Returns the m-value along the input route geometry
@@ -59,7 +78,7 @@ def get_line_mp(inputPolyline, lrs, rte_nm):
         endMP = get_mp_from_point(RouteGeom, endPt)
 
         return round(beginMP, 3), round(endMP, 3)
-    
+
     except Exception as e:
         print(e)
         return None, None
